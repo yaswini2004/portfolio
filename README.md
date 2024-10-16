6476f47a495812519b4ced0fcd81bf4582ecad3d
Tushar Bhardwaj <tusharbhardwaj2617@gmail.com>
2024-10-16 15:26:17 +0530

    init

---
+++ README.md
@@ -0,0 +1,94 @@
+# Portfolio Website 🌐
+
+
+![image](https://github.com/user-attachments/assets/3976cc2e-2204-46f6-bbea-886768d115c7)
+Welcome to my personal portfolio website repository! 
+
+## Table of Contents
+
+- [Introduction](#introduction)
+- [Features](#features)
+- [Tech Stack](#tech-stack)
+- [Live Demo](#live-demo)
+- [Quick Start](#quick-start)
+  - [Prerequisites](#prerequisites)
+  - [Cloning the Repository](#cloning-the-repository)
+  - [Installation](#installation)
+  - [Running the Project](#running-the-project)
+- [Contributing](#contributing)
+- [Links](#links)
+- [License](#license)
+- [Acknowledgements](#acknowledgements)
+
+## Introduction
+
+This project showcases my skills, projects, and experience through an interactive and visually engaging web application.
+
+## Features
+
+- **Hero Section**: Captivating introduction with a spotlight effect and dynamic background that draws attention.
+- **Bento Grid**: Modern layout presenting personal information with cutting-edge CSS design techniques for a clean and professional look.
+- **3D Elements**: Interactive 3D design elements, including a GitHub-style globe and card hover effects, adding depth and engagement to the user experience.
+- **Work Experience**: Prominent display of professional background to emphasize credibility and career progression.
+- **Canvas Effect**: Innovative use of HTML5 canvas to create visually striking effects in the "approaches" section, adding a unique touch to the presentation.
+- **Responsiveness**: Seamless adaptability across all devices, ensuring an optimal viewing experience for every user.
+
+## Tech Stack
+
+- **Next.js**: A React framework for server-side rendering, static site generation, and creating optimized web applications.
+- **Three.js**: A JavaScript library used to create and display animated 3D graphics in the browser.
+- **Framer Motion**: A popular library for animations and transitions in React applications, providing smooth and customizable animations.
+- **Tailwind CSS**: A utility-first CSS framework for creating custom, responsive designs quickly and efficiently.
+
+## Live Demo
+
+You can view the live version of my portfolio website at:  [Visit my Portfolio 🌐↗](https://tushar-bhardwaj.vercel.app/)
+
+## Quick Start
+
+Follow these steps to set up the project locally on your machine.
+
+### Prerequisites
+
+Ensure you have the following installed:
+
+- **Git**
+- **Node.js**
+- **npm** (Node Package Manager)
+
+### Cloning the Repository
+
+Clone the repository using Git:
+
+``` bash
+  git clone https://github.com/TuShArBhArDwA/your-portfolio-repo.git
+  cd your-portfolio-repo
+```
+
+### Installation
+Install the project dependencies:
+``` bash
+  npm install
+```
+
+### Running the Project
+Start the development server:
+
+``` bash
+  npm run dev
+```
+Open your browser and go to http://localhost:3000 to view the project.
+
+### Contributing
+Feel free to fork this repository and submit pull requests if you have suggestions or improvements. For bug reports or feature requests, please open an issue.
+
+### Links
+- **Old Portfolio:** [PortFolio](https://tusharbhardwaj.vercel.app/)
+- **LinkedIn:** [LinkedIn](https://www.linkedin.com/in/bhardwajtushar2004/)
+- **Connect 1:1** [Topmate](https://topmate.io/tusharbhardwaj)
+
+### License
+This project is licensed under the MIT License. See the LICENSE file for details.
+
+### Acknowledgements
+Thank you to all the open-source projects and tools that made this portfolio possible. Special thanks to the developers of Next.js, Three.js, Framer Motion, and Tailwind CSS.
---
+++ data/index.ts
@@ -0,0 +1,220 @@
+export const navItems = [
+    { name: "About", link: "#about" },
+    { name: "Projects", link: "#projects" },
+    { name: "Work Experience", link: "#work" },
+    { name: "Contact", link: "#contact" },
+  ];
+  
+  export const gridItems = [
+    {
+      id: 1,
+      title: "I prioritize client collaboration, fostering open communication ",
+      description: "",
+      className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+      imgClassName: "w-full h-full",
+      titleClassName: "justify-end",
+      img: "/b1.svg",
+      spareImg: "",
+    },
+    {
+      id: 2,
+      title: "I'm very flexible with time zone communications",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 3,
+      title: "My tech stack",
+      description: "I constantly try to improve",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-center",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 4,
+      title: "Tech enthusiast with a passion for development.",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "/grid.svg",
+      spareImg: "/b4.svg",
+    },
+  
+    {
+      id: 5,
+      title: "Currently building  a competitive programming platform",
+      description: "The Inside Scoop",
+      className: "md:col-span-3 md:row-span-2",
+      imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+      titleClassName: "justify-center md:justify-start lg:justify-center",
+      img: "/b5.svg",
+      spareImg: "/grid.svg",
+    },
+    {
+      id: 6,
+      title: "Do you want to start a project together?",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+      img: "",
+      spareImg: "",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 1,
+      title: "N-Queens Visualizer - Web Application",
+      des: "Unleash the power of strategy with this mesmerizing 3D visualization of the N-Queens problem, brought to life using Javascript",
+      img: "/p1.png",
+      iconLists: ["/html.png", "/css.jpeg", "/js.png" ],
+      link: "https://github.com/TuShArBhArDwA/N-Queen_Visualizer",
+    },
+    {
+      id: 2,
+      title: "TeleScript Caller - Backend Design",
+      des: "Streamline your communication with this Python script that automates phone calls by making API requests to a server with custom scripts.",
+      img: "/p2.png",
+      iconLists: ["python.jpeg"],
+      link: "https://github.com/TuShArBhArDwA/TeleScript-Caller",
+    },
+    {
+      id: 3,
+      title: "eVoteX-Voting_System - Web Design",
+      des: "Elevate your voting process with eVoteX—a secure and user-friendly terminal-based voting system in C, featuring encryption, authentication, and real-time vote counting for a reliable and engaging experience.",
+      img: "/p3.png",
+      iconLists: ["/c.svg"],
+      link: "https://github.com/TuShArBhArDwA/eVoteX-Online-Voting-System",
+    },
+    {
+      id: 4,
+      title: "Loan Calculator - Web Application",
+      des: "Developed a web application in Angular for calculating loan payments with simple interest, featuring an intuitive interface and visual graphs for clear, interactive results.",
+      img: "/p4.png",
+      iconLists: ["/html.png", "/css.jpeg", "/ts.svg"],
+      link: "https://github.com/TuShArBhArDwA/Loan-Calculator-Web-Application",
+    },
+  ];
+  
+  export const testimonials = [
+    {
+      quote:
+        "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+      name: "Michael Johnson",
+      title: "Director of AlphaStream Technologies",
+    },
+    {
+      quote:
+        "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+      name: "Michael Johnson",
+      title: "Director of AlphaStream Technologies",
+    },
+    {
+      quote:
+        "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+      name: "Michael Johnson",
+      title: "Director of AlphaStream Technologies",
+    },
+    {
+      quote:
+        "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+      name: "Michael Johnson",
+      title: "Director of AlphaStream Technologies",
+    },
+    {
+      quote:
+        "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+      name: "Michael Johnson",
+      title: "Director of AlphaStream Technologies",
+    },
+  ];
+  
+  export const companies = [
+    {
+      id: 1,
+      name: "cloudinary",
+      img: "/cloud.svg",
+      nameImg: "/cloudName.svg",
+    },
+    {
+      id: 2,
+      name: "appwrite",
+      img: "/app.svg",
+      nameImg: "/appName.svg",
+    },
+    {
+      id: 3,
+      name: "HOSTINGER",
+      img: "/host.svg",
+      nameImg: "/hostName.svg",
+    },
+    {
+      id: 4,
+      name: "stream",
+      img: "/s.svg",
+      nameImg: "/streamName.svg",
+    },
+    {
+      id: 5,
+      name: "docker.",
+      img: "/dock.svg",
+      nameImg: "/dockerName.svg",
+    },
+  ];
+  
+  export const workExperience = [
+    {
+      id: 1,
+      title: "Website Developer",
+      desc: "Working on client projects, developing websites and bots that enhanced user experience and functionality by designing intuitive, user-friendly interfaces.",
+      className: "md:col-span-2",
+      thumbnail: "/exp4.svg",
+    },
+    {
+      id: 2,
+      title: "Content Problem Reviewer and Solution Author - Leetcode",
+      desc: "Oversaw a quality control initiative for LeetCode problems, revising 120 problem statements and examples.",
+      className: "md:col-span-2", // change to md:col-span-2
+      thumbnail: "/exp2.svg",
+    },
+    // {
+    //   id: 3,
+    //   title: "Freelance App Dev Project",
+    //   desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
+    //   className: "md:col-span-2", // change to md:col-span-2
+    //   thumbnail: "/exp3.svg",
+    // },
+    {
+      id: 2,
+      title: "Technical Head - Apna College LPU",
+      desc: "Directing the technical team in developing and implementing innovative solutions for the Apna College LPU club.",
+      className: "md:col-span-2",
+      thumbnail: "/exp1.svg",
+    },
+  ];
+  
+  export const socialMedia = [
+    {
+      id: 1,
+      img: "/git.svg",
+      link: "https://github.com/TuShArBhArDwA",
+    },
+    {
+      id: 2,
+      img: "/twit.svg",
+      link: "https://twitter.com/TusharBhard2004",
+    },
+    {
+      id: 3,
+      img: "/link.svg",
+      link: "https://www.linkedin.com/in/bhardwajtushar2004/",
+    },
+  ];
