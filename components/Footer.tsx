df4278b2fb90007a06af5db31a2a9458a6cb1be0
B Yaswini Naidu <112719208+yaswini2004@users.noreply.github.com>
2024-10-16 16:32:31 +0530

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -14,7 +14,7 @@ const Footer = () => {
           Reach out to me today and let&apos;s discuss how I can help you
           achieve your goals.
         </p>
-        <a href="mailto:yaswiniyashu3@gmail.com?subject=Inquiry&body=Hello Yaswini," target="_blank">
+       <a href="mailto:yaswiniyashu3@gmail.com">
           <MagicButton
             title="Let's get in touch"
             icon={<FaLocationArrow />}
